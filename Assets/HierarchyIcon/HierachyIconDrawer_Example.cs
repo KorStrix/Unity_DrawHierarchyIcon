51f8a7f3a402479968725d02a18e1c26c139b9c2
Strix <35942253+KorStrix@users.noreply.github.com>
2019-04-26 22:11:33 +0900

    프로젝트 커밋

---
+++ Assets/HierarchyIcon/HierachyIconDrawer_Example.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HierachyIconDrawer_Example : MonoBehaviour, IDraw_HierarchyIcon
+{
+    public Texture2D[] arrNumberTexture;
+
+    public int _iCount;
+
+    void OnEnable()
+    {
+        StartCoroutine(CoUpdateNumber());
+    }
+
+    IEnumerator CoUpdateNumber()
+    {
+        _iCount = 0;
+
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+
+            _iCount++;
+        }
+    }
+
+
+    public void IDraw_HierarchyIcon_GetIcon(ref List<DrawIconInfo> listDrawIconInfo_DefaultCount_IsZero)
+    {
+        if (_iCount > 100)
+        {
+            int iNumber_Hundread = _iCount / 100;
+            if(iNumber_Hundread < arrNumberTexture.Length)
+                listDrawIconInfo_DefaultCount_IsZero.Add(new DrawIconInfo(arrNumberTexture[iNumber_Hundread], 100));
+        }
+
+
+        if (_iCount > 10)
+        {
+            int iNumber_Ten = _iCount / 10;
+            if (iNumber_Ten < arrNumberTexture.Length)
+                listDrawIconInfo_DefaultCount_IsZero.Add(new DrawIconInfo(arrNumberTexture[iNumber_Ten], 10));
+        }
+
+        int iNumber_One = _iCount % 10;
+        if (iNumber_One < arrNumberTexture.Length)
+            listDrawIconInfo_DefaultCount_IsZero.Add(new DrawIconInfo(arrNumberTexture[iNumber_One], 0));
+    }
+}
---
+++ Assets/HierarchyIcon/StrixLibrary/HierarchyIcon/HierachyIconConfig.cs
@@ -0,0 +1,101 @@
+﻿#region Header
+/*	============================================
+ *	작성자 : Strix
+ *	작성일 : 2019-04-24 오전 11:08:53
+ *	개요 : 
+   ============================================ */
+#endregion Header
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+using static HierachyIconList;
+
+#if ODIN_INSPECTOR
+using Sirenix.OdinInspector;
+#endif
+
+/// <summary>
+/// 
+/// </summary>
+[CreateAssetMenu(menuName = "StrixSO/" + nameof(HierachyIconConfig))]
+public class HierachyIconConfig : CSingletonSOBase<HierachyIconConfig>
+{
+    public HierachyIconList[] arrIconMapping;
+
+    static Dictionary<string, IconMappingDataBase> mapIconPer_Type = new Dictionary<string, IconMappingDataBase>();
+    static Dictionary<string, IconMappingDataBase> mapIconPer_Tag = new Dictionary<string, IconMappingDataBase>();
+
+    static public bool GetTexture_Per_Type(System.Type pType, ref Texture2D pTexture, ref int iOrder)
+    {
+        IconMappingDataBase sValue;
+        if (mapIconPer_Type.TryGetValue(pType.GetFriendlyName(), out sValue))
+        {
+            pTexture = sValue.GetTexture();
+            iOrder = sValue.iOrder;
+
+            return pTexture != null;
+        }
+        else
+        {
+            pTexture = null;
+            iOrder = 0;
+
+            return false;
+        }
+    }
+
+    static public bool GetTexture_Per_Tag(string strTag, ref Texture2D pTexture, ref int iOrder)
+    {
+        IconMappingDataBase sValue;
+        if (mapIconPer_Tag.TryGetValue(strTag, out sValue))
+        {
+            pTexture = sValue.GetTexture();
+            iOrder = sValue.iOrder;
+
+            return pTexture != null;
+        }
+        else
+        {
+            pTexture = null;
+            iOrder = 0;
+
+           return false;
+        }
+    }
+
+    protected override void OnAwake(bool bAppIsPlaying)
+    {
+        base.OnAwake(bAppIsPlaying);
+
+        UpdateIcon();
+    }
+
+#if ODIN_INSPECTOR
+    [Button]
+#endif
+    public void UpdateIcon()
+    {
+        mapIconPer_Type.Clear();
+        mapIconPer_Tag.Clear();
+
+        if (arrIconMapping == null)
+            return;
+
+        for (int i = 0; i < arrIconMapping.Length; i++)
+        {
+            for (int j = 0; j < arrIconMapping[i].p_listIconMappingData_Type.Count; j++)
+                mapIconPer_Type.Add(arrIconMapping[i].p_listIconMappingData_Type[j].strTypeName, arrIconMapping[i].p_listIconMappingData_Type[j]);
+
+            for (int j = 0; j < arrIconMapping[i].p_listIconMappingData_Tag.Count; j++)
+                mapIconPer_Tag.Add(arrIconMapping[i].p_listIconMappingData_Tag[j].strTag, arrIconMapping[i].p_listIconMappingData_Tag[j]);
+        }
+    }
+}
+#endif
